8b0895b3d7c6de431150ce781d5eaee4e08a201d
Yash-KK <reachyashk@gmail.com>
2025-04-07 00:16:00 +0530

    fix-imports

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,13 +1,8 @@
-import AppBar from "@/components/AppBar";
-import CareerOptions from "@/components/CareerOptions";
 import Dashboard from "@/components/Dashboard";
-import Intro from "@/components/Intro";
-import SideBar from "@/components/SideBar";
 
 export default function Playground() {
   return (
     <div>
-
       <Dashboard />
     </div>
   );
