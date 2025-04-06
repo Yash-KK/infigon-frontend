dbcdd370a4e297969f02eb8b8d3eac158046c301
Yash-KK <reachyashk@gmail.com>
2025-04-06 23:53:13 +0530

    add-sidebar-component

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,9 +1,14 @@
+import AppBar from "@/components/AppBar";
 import CareerOptions from "@/components/CareerOptions";
+import Dashboard from "@/components/Dashboard";
+import Intro from "@/components/Intro";
+import SideBar from "@/components/SideBar";
 
 export default function Playground() {
   return (
     <div>
-      <CareerOptions />
+
+      <Dashboard />
     </div>
   );
 }
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,15 +1,19 @@
+import DownloadIcon from "@/icons/Download";
+
 interface ButtonProps {
   text: string;
   className?: string;
   textClassName?: string;
   leftChild?: React.ReactNode
+  rightChild?: React.ReactNode
 }
 
-const Button = ({ text, className, textClassName, leftChild }: ButtonProps) => {
+const Button = ({ text, className, textClassName, leftChild, rightChild }: ButtonProps) => {
   return (
-    <div className={`${className} text-[#5B6871]`}>
+    <div className={`flex ${className} text-[#5B6871]`}>
         {leftChild}
       <span className={`${textClassName}`}>{text}</span>
+      {rightChild}
     </div>
   );
 };
