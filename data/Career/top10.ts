567f55ad01c614889be3ae37471e8fa479188e5f
Yash-KK <reachyashk@gmail.com>
2025-04-07 12:32:46 +0530

    complete career-motivators and top10

---
+++ data/Career/top10.ts
@@ -0,0 +1,55 @@
+export const motivators = [
+  {
+    text: "Achievement and Results",
+    border: "border-[#1999A1]",
+    bg: "bg-[#1999A1]",
+  },
+  { text: "Helping Others", border: "border-[#A15A19]", bg: "bg-[#A15A19]" },
+  { text: "Finalcial Rewards", border: "border-[#55DCDC]", bg: "bg-[#55DCDC]" },
+  {
+    text: "Creativity and Innovation",
+    border: "border-[#F6C61A]",
+    bg: "bg-[#F6C61A]",
+  },
+  {
+    text: "Security and Stability",
+    border: "border-[#9140F8]",
+    bg: "bg-[#9140F8]",
+  },
+  {
+    text: "Leadership and Influence",
+    border: "border-[#00A3FF]",
+    bg: "bg-[#00A3FF]",
+  },
+  {
+    text: "Continuous Learning",
+    border: "border-[#194CA1]",
+    bg: "bg-[#194CA1]",
+  },
+  { text: "Independence", border: "border-[#EE82FF]", bg: "bg-[#EE82FF]" },
+  {
+    text: "Teakwork and Collaboration",
+    border: "border-[#B97979]",
+    bg: "bg-[#B97979]",
+  },
+  {
+    text: "Flexibility and Work-Life Balance",
+    border: "border-[#FA9B56]",
+    bg: "bg-[#FA9B56]",
+  },
+];
+
+export const careerMotivatorsPurpose = [
+  "Enhanced Job Satisfaction",
+  "Improved Performance and Engagement",
+  "Career Planning and Development",
+];
+
+export const careerMotivatorsUnderstanding = [
+  "Alignment with Personal Values",
+  "Improved Decision-Making",
+  "Enhanced Self-Awareness",
+];
+
+export const description =
+  "Focuses on concrete, tangible information and present realities tangible information and present realities";
