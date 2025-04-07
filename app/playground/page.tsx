c1df618e18f159a616351893d6a65babf0dd2062
Yash-KK <reachyashk@gmail.com>
2025-04-07 16:38:06 +0530

    career-points

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,3 +1,15 @@
-export default function Playground() {
-  return <div></div>;
+import AppBar from "@/components/ui/AppBar";
+import CareerOptions from "@/components/CareerOptions";
+import Dashboard from "@/components/Dashboard";
+import Intro from "@/components/Intro";
+
+export default function Home() {
+  return (
+    <div>
+      <AppBar />
+      <Intro />
+      <CareerOptions />
+      <Dashboard />{" "}
+    </div>
+  );
 }
--- components/Career/Result.tsx
+++ components/Career/Result.tsx
@@ -6,6 +6,7 @@ import Button from "../ui/Button";
 import { tabs, description, careerData } from "@/data/Career/result";
 
 import CareerCard from "./CareerCard";
+import CareerPoints from "./CareerPoints";
 const CareerResult = () => {
   const [activeTab, setActiveTab] = useState("Your Scores");
 
@@ -70,10 +71,45 @@ const CareerResult = () => {
 
         {activeTab === "Conclusion" && (
           <>
-            <h3 className="text-lg font-semibold text-blue-600 mb-4">
+            <div className="flex items-center">
+              <p className="text-2xl font-semibold pr-5 text-[#0047AB]">
+                Summarised Result{" "}
+              </p>
+
+              <div className="h-12 w-0.5 rounded bg-gray-300 mr-5"></div>
+              <div className="flex">
+                <Button
+                  text="Low Relevance"
+                  className="flex items-center font-bold p-1 px-3 rounded-full text-[#696969]"
+                  leftChild={
+                    <span className="flex items-center justify-center font-bold bg-red-500 rounded-full w-8 h-8 mr-2"></span>
+                  }
+                />
+                <Button
+                  text="Moderate Relevance"
+                  className="flex items-center font-bold p-1 px-3 rounded-full text-[#696969]"
+                  leftChild={
+                    <span className="flex items-center justify-center font-bold bg-yellow-500 rounded-full w-8 h-8 mr-2"></span>
+                  }
+                />
+                <Button
+                  text="High Relevance"
+                  className="flex items-center font-bold p-1 px-3 rounded-full text-[#696969]"
+                  leftChild={
+                    <span className="flex items-center justify-center font-bold bg-green-500 rounded-full w-8 h-8 mr-2"></span>
+                  }
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {careerData.map((item, idx) => (
+                <CareerPoints key={idx} data={item} index={idx} />
+              ))}
+            </div>
+            <h3 className="text-lg font-semibold text-[#0047AB] mb-4">
               Conclusion
             </h3>
-            <div className="space-y-4 bg-red-400 rounded-xl p-2 text-white font-medium">
+            <div className="space-y-4 bg-yellow-600 rounded-xl p-2 text-white font-medium">
               {description}
             </div>
           </>
