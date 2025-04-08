06ffe4879cec5dbea2f51272dd1418c20b631f81
Yash-KK <reachyashk@gmail.com>
2025-04-08 11:31:33 +0530

    build understanding-learnings

---
+++ components/Learning/HoverCard.tsx
@@ -0,0 +1,29 @@
+import Image, { StaticImageData } from "next/image";
+
+interface HoverCardProps {
+  color: string;
+  title: string;
+  description: string;
+  icon: StaticImageData;
+}
+
+const HoverCard = ({ color, title, description, icon }: HoverCardProps) => {
+  return (
+    <div className="relative w-44 h-52 group cursor-pointer transition-all duration-300">
+      <div className="absolute inset-0 flex items-center justify-center group-hover:opacity-0 transition-opacity duration-300">
+        <Image src={icon} alt={title} width={100} height={100} />
+      </div>
+
+      <div
+        className={`absolute inset-0 scale-0 group-hover:scale-100 transition-transform duration-300 rounded-xl p-4 text-white flex flex-col items-center justify-center text-center`}
+        style={{ backgroundColor: color }}
+      >
+        <Image src={icon} alt={title} width={30} height={30} />
+        <div className="mt-2 font-bold text-lg">{title.toUpperCase()}</div>
+        <p className="text-sm mt-1">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HoverCard;
